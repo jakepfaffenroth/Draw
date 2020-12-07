ee99b23d987e2cb18be7527e155348291675de49
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-12-07 13:12:01 -0800

    Added ability to fill in enclosed areas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var gridSize = 16;
+var gridSize = 20;
 var r = 100; // These
 var g = 100; // set color
 var b = 100; // to black
@@ -8,9 +8,12 @@ const html = document.querySelector('HTML');
 const container = document.querySelector('#container');
 const textBox = document.querySelector('.textBox');
 const gridBtn = document.querySelector('#gridButton');
+const fillBtn = document.querySelector('#fillButton');
 const randColorBtn = document.querySelector('#colorButton');
 const pickColorBtn = document.querySelector('#customColor');
 const tBox = document.querySelector('.textBox');
+
+let fillMode = false;
 
 // Stop drawing whenever cursor is lifted, no matter where cursor is located
 let mousedown = false;
@@ -116,25 +119,74 @@ function createGrid() {
   for (let i = 0; i < gridSize; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('gridSquare');
+    gridSquare.id = i.toString();
     gridSquare.style.width = squareSize + 'vmin';
     gridSquare.style.height = squareSize + 'vmin';
     gridSquare.classList.remove('filledBox');
     container.appendChild(gridSquare);
   }
 
+  const allGridSquares = document.querySelectorAll('.gridSquare');
   // Hover effects
 
-  const allGridSquares = document.querySelectorAll('.gridSquare');
   document.querySelector('.gridSquare');
 
   allGridSquares.forEach((box) => {
+    box.dataset.posY = Math.floor(parseInt(box.id) / Math.sqrt(gridSize));
+    box.dataset.posX = Math.floor(parseInt(box.id) - Math.sqrt(gridSize) * box.dataset.posY);
+
+    box.addEventListener('click', function () {
+      if (fillMode) {
+        fill({ x: box.dataset.posX, y: box.dataset.posY });
+      }
+    });
     box.addEventListener('mousedown', function () {
       mousedown = true;
-    });
-
-    box.addEventListener('mouseover', function () {
-      if (mousedown) {
+      if (!fillMode) {
         box.style.backgroundColor = color;
+      }
+    });
+    box.addEventListener('mouseover', function () {
+      if (mousedown && !fillMode) {
+        box.style.backgroundColor = color;
+      }
+    });
+  });
+}
+
+// Fill mode
+fillBtn.addEventListener('click', function () {
+  fillMode = !fillMode;
+  if (fillMode) {
+    fillBtn.style.color = 'red';
+  } else {
+    fillBtn.style.color = null;
+  }
+});
+
+function fill({ x, y }) {
+  const allGridSquares = document.querySelectorAll('.gridSquare');
+  const gridRoot = Math.sqrt(gridSize);
+  const kernel = [
+    { x, y },
+    { x: x - 1, y: y - 1 },
+    { x, y: y - 1 },
+    { x: x + 1, y: y - 1 },
+    { x: x - 1, y },
+    { x: x + 1, y },
+    { x: x - 1, y: y + 1 },
+    { x, y: y + 1 },
+    { x: x + 1, y: y + 1 },
+  ].filter((box) => box.x >= 0 && box.x < gridRoot && box.y >= 0 && box.y < gridRoot);
+  allGridSquares.forEach((box) => {
+    if (box.style.backgroundColor) {
+      return;
+    }
+
+    kernel.forEach((coord) => {
+      if (parseInt(box.dataset.posX) === coord.x && parseInt(box.dataset.posY) === coord.y) {
+        box.style.backgroundColor = color;
+        fill({ x: coord.x, y: coord.y });
       }
     });
   });
